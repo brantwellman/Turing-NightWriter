f2a624fb6d196ee33289719a37f043e957a5d6e0
Brant Wellman <BrantWellman@gmail.com>
2015-10-20 16:47:39 -0600

    Written skeleton tests for functionality - Skipped

--- test/nightwriter_test.rb
+++ test/nightwriter_test.rb
@@ -2,6 +2,57 @@ require 'minitest'
 require 'minitest/autorun'
 require_relative '../lib/nightwriter'
 
-class NightWriterTest < Minitest::testing
+class NightWriterTest < Minitest::Test
+
+  def test_that_it_exists
+    writer = NightWriter.new
+    assert writer
+  end
+
+  def test_it_adds_a_cap_to_output_string
+    skip
+    writer = NightWriter.new
+  end
+
+  def test_it_adds_cap_shift_to_ouput
+    skip
+    writer = NightWriter.new
+  end
+
+  def test_it_does_not_add_cap_shift_to_punctuation
+    skip
+    writer = NightWriter.new
+  end
+
+  def test_it_does_not_add_cap_shift_to_downcase_letter
+    skip
+    writer = NightWriter.new
+  end
+
+  def test_it_returns_true_for_a_cap
+    skip
+    writer = NightWriter.new
+  end
+
+  def test_it_returns_false_for_a_lowercase
+    skip
+    writer = NightWriter.new
+  end
+
+  def test_it_returns_false_for_a_cap_for_punctuation
+    skip
+    writer = NightWriter.new
+  end
+
+  def test_it_returns_a_braille_encoded_string
+    skip
+    writer = NightWriter.new
+  end
+
+
+
+
+
+
 
 end
