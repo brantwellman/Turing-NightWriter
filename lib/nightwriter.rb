938f6d1765a2a9e42095fd9973b903fa5454f907
Brant Wellman <BrantWellman@gmail.com>
2015-10-21 17:03:02 -0600

    All tests passing. Fully functional

--- lib/nightwriter.rb
+++ lib/nightwriter.rb
@@ -1,46 +1,40 @@
 require 'pry'
 require './alphabet_eng_to_braille.rb'
 
-class FileReader
-  def read
-    filename = ARGV[0]
-    File.read(filename)
-  end
-end
-
 class NightWriter
   attr_reader :file_reader, :input
 
-  def initialize(input_file_name, output_file_name)
-    @output_file = File.open(output_file_name, "w")
-    @input = File.read(input_file_name)
+  def initialize(input_file_name = nil, output_file_name = nil)
     @outputs = ["", "", ""]
-    encode_to_braille(@input)
-    write
-  end
-
-  def write
-    output_print = []
-    str_length = @outputs[0].length
-    iterations = str_length/80 + 1
-    remainders = str_length % 80
-    iterations.times do |n|
-      @outputs.each do |output|
-        @output_file.write(output[80*n..79+80*n] + "\n")
-      end
+    @print_string = ""
+    if input_file_name != nil
+      @input = File.read(input_file_name)
+      @output_file = File.open(output_file_name, "w")
+      encode_to_braille(@input)
+      write(@print_string)
+      puts "Created '#{output_file_name}' containing #{count_characters_in_file(input_file_name)} characters."
     end
   end
 
-  def create_output_file_from_input_file(input_filename, output_filename)
-    @input = File.open(input_filename)
-    # string = @input.read * 3
-    output = File.open(output_filename, "w")
-    output.write(string)
-    puts "Created '#{output_filename}' containing #{count_characters_in_file(input_filename)} characters."
+  def write(print_string)
+    @output_file.write(print_string)
+    # output_print = []
+    # str_length = @outputs[0].length
+    # iterations = str_length/80 + 1
+    # remainders = str_length % 80
+    # iterations.times do |n|
+    #   @outputs.each do |output|
+    #     @output_file.write(output[80*n..79+80*n] + "\n")
+    #   end
+    # end
   end
 
-  def count_characters_in_file(input_filename)
-    lines = File.readlines(input_filename)
+  def strip_new_line_characters(input)
+    input.gsub("\n", "")
+  end
+
+  def count_characters_in_file(input_file_name)
+    lines = File.readlines(input_file_name)
     total_characters = lines.join.size
   end
 
@@ -48,34 +42,55 @@ class NightWriter
     "A" <= char && char <= "Z"
   end
 
-  def add_cap_letter_shift_and_cap(char)
+  def add_cap_letter_and_shift_to_output(char)
     @outputs[0] = @outputs[0] + (ALPHABET.fetch("cap")[0..1])
     @outputs[1] = @outputs[1] + (ALPHABET.fetch("cap")[2..3])
     @outputs[2] = @outputs[2] + (ALPHABET.fetch("cap")[4..5])
     @outputs[0] = @outputs[0] + (ALPHABET.fetch(char.downcase)[0..1])
     @outputs[1] = @outputs[1] + (ALPHABET.fetch(char.downcase)[2..3])
     @outputs[2] = @outputs[2] + (ALPHABET.fetch(char.downcase)[4..5])
+    @outputs
   end
 
   def add_non_cap_character_to_ouput(char)
     @outputs[0] = @outputs[0] + ALPHABET.fetch(char)[0..1]
     @outputs[1] = @outputs[1] + ALPHABET.fetch(char)[2..3]
     @outputs[2] = @outputs[2] + ALPHABET.fetch(char)[4..5]
+    @outputs
   end
 
   def encode_to_braille(input)
-    input.gsub("\n", "").each_char do |char|
+    strip_new_line_characters(input).each_char do |char|
       if capital_letter?(char)
-        add_cap_letter_shift_and_cap(char)
+        add_cap_letter_and_shift_to_output(char)
       else
         add_non_cap_character_to_ouput(char)
       end
     end
-    @outputs
+    # @outputs
+    convert_output_array_to_one_string(@outputs)
   end
 
+  def convert_output_array_to_one_string(outputs)
+    # outputs.join("\n") + "\n"
+
+    until outputs[0] == "" do
+      outputs.each do |output|
+        @print_string << output.slice!(0..79) + "\n"
+      end
+    end
+    @print_string
+  end
+
+  # iterate through each string in outputs
+  #   take first 80 characters and add a \n character to the end
+  # =>add this character to new output string
+      # continue through each string
+      # until each string is empty
+
+
+
 # split output strings into lengths of 80
-# create proper output to terminal "Created file X with x characters"
 # Tests!
 
  # def encode_file_to_braille
--- test/nightwriter_test.rb
+++ test/nightwriter_test.rb
@@ -3,7 +3,6 @@ require 'minitest/autorun'
 require_relative '../lib/nightwriter'
 
 class NightWriterTest < Minitest::Test
-
   def test_that_it_exists
     writer = NightWriter.new
     assert writer
@@ -11,82 +10,69 @@ class NightWriterTest < Minitest::Test
 
   def test_that_it_checks_for_capital_letters
     writer = NightWriter.new
-
     assert writer.capital_letter?("A")
   end
 
   def test_that_it_refutes_capital_letters
     writer = NightWriter.new
-
     refute writer.capital_letter?("b")
   end
 
   def test_that_it_refutes_punctuation_as_capital_letters
     writer = NightWriter.new
-
     refute writer.capital_letter?("!")
   end
 
-  def test_it_adds_a_cap_to_output_strings
-    skip
+  def test_it_adds_a_cap_and_shift_to_output_strings
     writer = NightWriter.new
-
-    assert_equal  , writer.add_cap_letter_shift_and_cap("A")
+    outputs = writer.add_cap_letter_and_shift_to_output("A")
+    assert_equal "..0.", outputs[0]
+    assert_equal "....", outputs[1]
+    assert_equal ".0..", outputs[2]
   end
-
-  def test_it_adds_cap_shift_to_ouput
-    skip
-    writer = NightWriter.new
-  end
-
-
-  def add_cap_letter_shift_and_cap(char)
-    @out_str1 << ALPHABET.fetch("cap")[0..1]
-    @out_str2 << ALPHABET.fetch("cap")[2..3]
-    @out_str3 << ALPHABET.fetch("cap")[4..5]
-    @out_str1 << ALPHABET.fetch(char.downcase)[0..1]
-    @out_str2 << ALPHABET.fetch(char.downcase)[2..3]
-    @out_str3 << ALPHABET.fetch(char.downcase)[4..5]
-    # output_string << ALPHABET.fetch("cap")
-    # output_string << char.downcase
-  end
-
-
 
   def test_it_does_not_add_cap_shift_to_punctuation
-    skip
     writer = NightWriter.new
+    outputs = writer.add_non_cap_character_to_ouput("?")
+    assert_equal "..", outputs[0]
+    assert_equal "0.", outputs[1]
+    assert_equal "00", outputs[2]
+
   end
 
   def test_it_does_not_add_cap_shift_to_downcase_letter
-    skip
     writer = NightWriter.new
+    outputs = writer.add_non_cap_character_to_ouput("a")
+    assert_equal "0.", outputs[0]
+    assert_equal "..", outputs[1]
+    assert_equal "..", outputs[2]
   end
 
-  def test_it_returns_true_for_a_cap
-    skip
+  def test_it_strips_newline_characters_from_input
     writer = NightWriter.new
+    assert_equal "bc!", writer.strip_new_line_characters("bc!\n")
   end
 
-  def test_it_returns_false_for_a_lowercase
-    skip
+  def test_it_converts_output_array_less_than_80_characters_to_one_string
     writer = NightWriter.new
+    assert_equal "000\n...\n000\n", writer.convert_output_array_to_one_string(["000", "...", "000"])
   end
 
-  def test_it_returns_false_for_a_cap_for_punctuation
-    skip
+  def test_it_conversts_output_array_of_more_than_80_characters_to_one_string
     writer = NightWriter.new
+    expected = <<-EOF
+00000000000000000000000000000000000000000000000000000000000000000000000000000000
+................................................................................
+00000000000000000000000000000000000000000000000000000000000000000000000000000000
+0000000000
+..........
+0000000000
+EOF
+    assert_equal expected, writer.convert_output_array_to_one_string(["000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", "..........................................................................................", "000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"])
   end
 
   def test_it_returns_a_braille_encoded_string
     skip
     writer = NightWriter.new
   end
-
-
-
-
-
-
-
 end
