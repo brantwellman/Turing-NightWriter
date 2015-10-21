de279e951a2832df5d09e2d21e58bd33c98b9e4a
Brant Wellman <BrantWellman@gmail.com>
2015-10-21 08:06:14 -0600

    Cleaned up nightwriter test file - still needs tests

--- test/nightwriter_test.rb
+++ test/nightwriter_test.rb
@@ -9,15 +9,49 @@ class NightWriterTest < Minitest::Test
     assert writer
   end
 
-  def test_it_adds_a_cap_to_output_string
+  def test_that_it_checks_for_capital_letters
+    writer = NightWriter.new
+
+    assert writer.capital_letter?("A")
+  end
+
+  def test_that_it_refutes_capital_letters
+    writer = NightWriter.new
+
+    refute writer.capital_letter?("b")
+  end
+
+  def test_that_it_refutes_punctuation_as_capital_letters
+    writer = NightWriter.new
+
+    refute writer.capital_letter?("!")
+  end
+
+  def test_it_adds_a_cap_to_output_strings
     skip
     writer = NightWriter.new
+
+    assert_equal  , writer.add_cap_letter_shift_and_cap("A")
   end
 
   def test_it_adds_cap_shift_to_ouput
     skip
     writer = NightWriter.new
   end
+
+
+  def add_cap_letter_shift_and_cap(char)
+    @out_str1 << ALPHABET.fetch("cap")[0..1]
+    @out_str2 << ALPHABET.fetch("cap")[2..3]
+    @out_str3 << ALPHABET.fetch("cap")[4..5]
+    @out_str1 << ALPHABET.fetch(char.downcase)[0..1]
+    @out_str2 << ALPHABET.fetch(char.downcase)[2..3]
+    @out_str3 << ALPHABET.fetch(char.downcase)[4..5]
+    # output_string << ALPHABET.fetch("cap")
+    # output_string << char.downcase
+  end
+
+
 
   def test_it_does_not_add_cap_shift_to_punctuation
     skip
