d10ad5ad6fa040c62dad2e7c8ec5a83a9b8aa26a
Brant Wellman <BrantWellman@gmail.com>
2015-10-21 21:59:24 -0600

    Takes array of strings representing each line and concatenates them into three strings in the array that represent 3 full lines of english characters (top, middle, bottom)

--- lib/nightreader.rb
+++ lib/nightreader.rb
@@ -6,19 +6,11 @@ class NightReader
     if input_file_name != nil
       @input = File.read(input_file_name)
       @output_file = File.open(output_file_name, "w")
-      @string1 = ""
-      @string2 = ""
-      @string3 = ""
-      @string_array3 = ["", "", ""]
-      @full_string_array = []
       encode_to_english(@input)
       # write(@input)
       puts "Created '#{output_file_name}' containing #{count_characters_in_file(input_file_name)} characters."
     end
-
-
-
-    @input = "ksdurejsks\nsieEDDSkij\nxkdliea:sw\n"
+    @concated_arr = ["", "", ""]
   end
 
   def count_characters_in_file(output_file_name)
@@ -26,26 +18,48 @@ class NightReader
     total_characters = lines.join.size
   end
 
-  def write(input)
-    @output_file.write(input)
+  def write(output_string)
+    @output_file.write(output_string)
   end
 
   def split_string_at_new_lines(input_string)
     input_string.split("\n")
   end
 
-  # destructively add string from array to new string
-  # iterate through 3 strings
-  # def concatenate_every_third_string(array)
+  def concatenate_every_third_string(array)
+    array.each do |str|
+      if array.index(str) % 3 == 0
+        @concated_arr[0] << str
+      elsif array.index(str) % 3 == 1
+        @concated_arr[1] << str
+      elsif array.index(str) % 3 == 2
+        @concated_arr[2] << str
+      end
+    end
+    @concated_arr
+  end
+
+  # def create_strings_of_letters
+  #   # until @concated_arr[0] == "" do
   #
+  #     @concated_arr.each do |str|
+  #      str.slice!(0..2)
+  #     end
+  #     first_letter
+  #   # end
   # end
-  #
-  def encode_to_english(input)
-    split_string_at_new_lines(input)
+
+  def encode_to_english(string)
+    split_string_at_new_lines(string)
+    concatenate_every_third_string(array)
+    create_strings_of_letters
   end
 
 end
 
 reader = NightReader.new
 # reader = NightReader.new(ARGV[0], ARGV[1])
-p reader.split_string_at_new_lines("ksdurejsks\nsieEDDSkij\nxkdliea:sw\n")
+# p reader.split_string_at_new_lines("00.0.000..\n.0..00.0..\n000.......\n")
+# array_test = ["1000000000000000", "2..............", "30000000000000", "4..............", "50000000000000", "6.............."]
+# p reader.concatenate_every_third_string(array_test)
+# p reader.create_strings_of_letters
--- test/nightreader_test.rb
+++ test/nightreader_test.rb
@@ -10,7 +10,25 @@ class NightReaderTest < Minitest::Test
 
   def test_it_splits_incoming_string_on_new_line_characters
     reader = NightReader.new
-    string = "This is\nso much\nfun!!!!"
-    assert_equal ["This is", "so much", "fun!!!!"], reader.split_string_at_new_lines(string)
+    string = "0000...\n....000\n0000..."
+    assert_equal ["0000...", "....000", "0000..."], reader.split_string_at_new_lines(string)
   end
+
+  def test_it_concatenates_every_third_string_into_3_strings
+    reader = NightReader.new
+    array = ["1000", "2000", "3000", "4000", "5000", "6000", "7000", "8000", "9000"]
+    assert_equal ["100040007000", "200050008000", "300060009000"], reader.concatenate_every_third_string(array)
+  end
+
+  def test_concatenator_works_with_only_three_strings
+    reader = NightReader.new
+    array = ["1000", "2000", "3000"]
+    assert_equal ["1000", "2000", "3000"], reader.concatenate_every_third_string(array)
+  end
+
+  # def test_it_creates_arrays_from_the_first_two_elements_of_each_string
+  #   reader = NightReader.new
+  #   array = ["100435", "200055", "300088"]
+  #   assert_equal ["102030", "040000", "355588"], reader.create_strings_of_letters
+  # end
 end
